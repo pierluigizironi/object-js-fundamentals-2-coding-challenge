36d0f20ef6aa9479a442d7af6ba8f4d6304cc17d
pierluigizironi <zironipierluigi@gmail.com>
2022-05-28 19:43:13 +0200

    add john and miller objects

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ height (Mark Miller and John Smith)
 method on both objects). Store the BMI value to a property, and also return it
 from the method
 3. Log to the console who has the higher BMI, together with the full name and the
-respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 tall.
 */
+
+const john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    weight: 78,
+    height: 1.69,
+}
+
+const mark = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    weight: 92,
+    height: 1.95,
+}
